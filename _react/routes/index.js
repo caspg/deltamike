adbaaff85b2b489f3ba68d0d9a8560d4e5ae4d3b
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-26 19:29:53 +0100

    use hashHistory

--- _react/routes/index.js
+++ _react/routes/index.js
@@ -4,8 +4,10 @@ import { Route } from 'react-router';
 import App from '../containers/App';
 import UploadPage from '../containers/UploadPage';
 
+const baseurl = '/deltamike';
+
 export default (
-  <Route path="/editor" component={App}>
+  <Route path={`${baseurl}`} component={App}>
     <Route path="/upload" component={UploadPage} />
   </Route>
 );
